71eb850744bcb65bf0281000de677004272c2e10
Richard Nguyen <richardng1311@gmail.com>
2024-03-25 18:21:03 -0400

    created an additional route for POST request

--- src/book/routes.js
+++ src/book/routes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/', controller.getBooks);
 router.get('/isbn/:isbn', controller.getBookByISBN);
 router.get('/id/:bid', controller.getBookById);
+router.post('/', controller.addBook);
 
 module.exports = router;
