464350caddcc530e3b9ede9448fbc0fca3b2a032
Richard Nguyen <richardng1311@gmail.com>
2024-03-25 18:19:52 -0400

    corrected retrieve books query and implemented add book

--- src/book/queries.js
+++ src/book/queries.js
@@ -1,18 +1,17 @@
-const getBooks = `
-  SELECT b.*, g.name 
-  FROM books AS b 
-  CROSS JOIN book_genres AS bg 
-  CROSS JOIN genres AS g 
-  WHERE b.bid = bg.book_id 
-  AND bg.genre_id = g.gid
-`;
+const getBooks = 'SELECT * FROM books b';
 
-const getBookById = 'SELECT * FROM books as b WHERE b.bid = $1';
+const getBookById = 'SELECT * FROM books b WHERE b.bid = $1';
 
-const getBookByISBN = 'SELECT * FROM books as b WHERE b.isbn = $1';
+const getBookByISBN = 'SELECT * FROM books b WHERE b.isbn = $1';
+
+const checkISBNExists = 'SELECT b FROM books b WHERE b.isbn = $1';
+
+const addBook = 'INSERT INTO books (title, author, isbn, publication_year, description, quantity_available) VALUES ($1, $2, $3, $4, $5, $6)';
 
 module.exports = {
   getBooks,
   getBookById,
   getBookByISBN,
+  checkISBNExists,
+  addBook,
 };
